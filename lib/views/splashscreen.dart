b5780d2a43c04f20bb09f9c92bd17aafa17be3a6
dapen <velia.sagita.tif17@polban.ac.id>
2022-01-07 21:06:49 +0700

    test splashscreen

--- lib/views/splashscreen.dart
+++ lib/views/splashscreen.dart
@@ -1,17 +1,40 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'kategori.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreen createState() => _MySplashScreen();
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     decoration: BoxDecoration(color: Colors.white),
+  //     child: Stack(
+  //       children: <Widget>[
+  //         Center(
+  //             //     // child: SvgPicture,
+  //             )
+  //       ],
+  //     ),
+  //   );
+  // }
+}
+
+class _MySplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Kategori())));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Stack(
-        children: <Widget>[
-          Center(
-              //     // child: SvgPicture,
-              )
-        ],
-      ),
-    );
+        color: Colors.white,
+        // child: Image(image: "../assets/logo-bio.png")
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
   }
 }
