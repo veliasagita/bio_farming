4c1d83f4957d84cf7ac8c57f038d9e39751fe3a5
dapen <velia.sagita.tif17@polban.ac.id>
2022-01-18 20:58:31 +0700

    backend config

--- lib/views/kategori.dart
+++ lib/views/kategori.dart
@@ -1,10 +1,46 @@
+// import 'package:flutter/material.dart';
+
+// class Kategori extends StatelessWidget {
+//   const Kategori({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("Bio Farming")),
+
+//     );
+//   }
+// }
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Kategori extends StatelessWidget {
+class Kategori extends StatefulWidget {
+  const Kategori({Key? key}) : super(key: key);
+
+  @override
+  _KategoriState createState() => _KategoriState();
+}
+
+class _KategoriState extends State<Kategori> {
+  final firestoreInstance = FirebaseFirestore.instance;
   @override
   Widget build(BuildContext context) {
+    var list = [];
+    firestoreInstance.collection("kategori").get().then((querySnapshot) {
+      // print(querySnapshot.docs.sublist(0));
+      // print(querySnapshot);
+      // querySnapshot.docs.forEach((result) {
+        // print(result.data());
+        // Iterable value = result.data().values;
+        // list.add(value); //ini blm masuk array
+      // });
+      
+    });
     return Scaffold(
-      appBar: AppBar(title: Text("Bio Farming")),
+      appBar: AppBar(title: const Text("Bio Farming")),
+      // body: AnimatedList(
+
+      // ),
     );
   }
 }
--- lib/views/splashscreen.dart
+++ lib/views/splashscreen.dart
@@ -3,21 +3,10 @@ import 'package:flutter/material.dart';
 import 'kategori.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
   @override
   _MySplashScreen createState() => _MySplashScreen();
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Container(
-  //     decoration: BoxDecoration(color: Colors.white),
-  //     child: Stack(
-  //       children: <Widget>[
-  //         Center(
-  //             //     // child: SvgPicture,
-  //             )
-  //       ],
-  //     ),
-  //   );
-  // }
 }
 
 class _MySplashScreen extends State<SplashScreen> {
@@ -25,16 +14,29 @@ class _MySplashScreen extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => Kategori())));
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const Kategori())));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.white,
-        // child: Image(image: "../assets/logo-bio.png")
-        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+    return Stack(
+      children: [
+        Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/logo-bio.png'),
+                    alignment: Alignment.center))),
+        const Scaffold(
+          backgroundColor: Colors.white,
+        )
+      ],
+    );
+    // return Container(
+    //     decoration: const BoxDecoration(
+    //         image: DecorationImage(
+    //             image: AssetImage('assets/logo-bio.png'),
+    //             alignment: Alignment.center)));
   }
 }
