ad9f08327129e362325a93d963102e0952f9e447
Giusty-Fadh-Wiranapoera <wiranapoera.fadh@gmail.com>
2022-01-27 10:20:05 +0700

    Update kategori.dart

--- lib/views/kategori.dart
+++ lib/views/kategori.dart
@@ -1,16 +1,14 @@
 import 'package:bio_farming/views/tanaman.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:stacked_card_carousel/stacked_card_carousel.dart';
 
 class Kategori extends StatelessWidget {
   const Kategori({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     final db = FirebaseFirestore.instance;
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: StreamBuilder<QuerySnapshot>(
           stream: db.collection('kategori').snapshots(),
           builder: (context, snapshot) {
@@ -18,30 +16,22 @@ class Kategori extends StatelessWidget {
               return const Center(
                 child: CircularProgressIndicator(),
               );
-            } 
-            else {
-              return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: snapshot.data!.docs.map((doc) {
-                      return Container(
-                        width: 250,
-                        height: 250,
-                        child: ListTile(
-                          title: Text(doc.get('nama') ?? doc.get('nama')),
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        Tanaman(doc.get('nama'), doc.id)));
-                          },
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                ),
+            } else {
+              return ListView(
+                children: snapshot.data!.docs.map((doc) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(doc.get('nama') ?? doc.get('nama')),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    Tanaman(doc.get('nama'), doc.id)));
+                      },
+                    ),
+                  );
+                }).toList(),
               );
             }
           }),
