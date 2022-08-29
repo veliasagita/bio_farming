e7c988e281e51b6b858fe0f199c067235e5f3328
VeliaSagita <velia.sagita.tif17@polban.ac.id>
2022-08-29 10:15:49 +0700

    add flutter_launcher_icon

--- lib/views/tanaman.dart
+++ lib/views/tanaman.dart
@@ -44,22 +44,19 @@ class Tanaman extends StatelessWidget {
                                 width: MediaQuery.of(context).size.width / 3,
                                 padding: const EdgeInsets.all(20),
                                 decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Colors.grey.shade300,
-                                      //get image
-                                    // image: DecorationImage(
-                                    //   fit: BoxFit.cover,
-                                    //   image: AssetImage(doc.get('image')),
-                                    // ),
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.grey.shade300,
                                 ),
-                                child: Center(
-                                  child: Text(
-                                      doc.get('nama') ?? doc.get('nama'),
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18),
-                                      textAlign: TextAlign.center),
-                                )),
+                                child: Column(children: [
+                                  Center(
+                                    child: Text(
+                                        doc.get('nama') ?? doc.get('nama'),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18),
+                                        textAlign: TextAlign.center),
+                                  ),
+                                ])),
                             onTap: () {
                               Navigator.push(
                                   context,
